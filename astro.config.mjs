5d6fec4a87db244f1e7462902e54c5fd895c519a
Bosco <bosco@Mac.lan>
2026-07-28 11:34:59 +0800

    fix: configure custom help center domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://boscohgit.github.io",
-  base: "/m365-help-center",
+  site: "https://help.incorp-int.com",
+  base: "/",
   output: "static",
   integrations: [sitemap()],
 });
